926a4bcfe34a25d8bd44ab3aebb70d6b0229b961
Alex <ais_alex@ukr.net>
2023-05-07 21:23:59 +0300

    update app.js

--- src/App/App.js
+++ src/App/App.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 import { CurrencyForm } from "../components/CurrencyForm/CurrencyForm";
 import Loader from "components/Loder/Loader";
@@ -6,61 +6,92 @@ import { searchRates } from "services/api";
 
 import "./App.css";
 
-const initialState = {
-  fromCurrency: "UAH",
-  toCurrency: "USD",
-  price: 0,
-  toPrice: 0,
-};
+// const initialState = {
+//   fromCurrency: "UAH",
+//   toCurrency: "USD",
+//   fromPrice: 0,
+//   toPrice: 0,
+// };
 
 function App() {
   const [fromCurrency, setFromCurrency] = useState("UAH");
   const [toCurrency, setToCurrency] = useState("USD");
+
   const [fromPrice, setFromPrice] = useState(0);
   const [toPrice, setToPrice] = useState(0);
+
   const [rates, setRates] = useState([]);
+  // const [rates2, setRates2] = useState([]);
   const [loading, setLoading] = useState();
   const [error, setError] = useState();
 
+  const isFirstRender = useRef(true);
+
+  console.log(fromCurrency);
+  console.log(toCurrency);
+
   useEffect(() => {
     const fetchRates = async () => {
-      if (rates.length === 0) {
-        try {
-          setLoading(true);
-          const result = await searchRates();
-          setRates(result);
-        } catch (error) {
-          setError(error.data.message);
-          console.log(error);
-          alert("Oooops, something went wrong :(");
-        } finally {
-          setLoading(false);
-          console.log(rates);
-        }
+      // if (rates.length === 0) {
+      try {
+        setLoading(true);
+        const result = await searchRates(fromCurrency);
+        // const result2 = await searchRates(toCurrency);
+        setRates(result);
+        // setRates2(result2);
+      } catch (error) {
+        setError(error.data.message);
+        console.log(error);
+        alert("Oooops, something went wrong :(");
+      } finally {
+        setLoading(false);
+        // console.log(rates);
+        // console.log(rates2);
       }
     };
+    // };
 
     fetchRates();
-  }, [rates]);
+  }, [fromCurrency]);
+
+  useEffect(() => {
+    if (isFirstRender.current) {
+      isFirstRender.current = false;
+      return;
+    }
+
+    onChangeFromPrice(fromPrice);
+  }, [toCurrency]);
 
   const onChangeFromPrice = (value) => {
-    // const price = rates.cc[fromCurrency] / value;
-    // const result = price * rates.cc[toCurrency];
+    // const price = rates.conversion_rates[toCurrency] / value;
+    const result = value * rates.conversion_rates[toCurrency];
 
-    console.log(value);
-    console.log(rates.cc);
-    // console.log(result);
+    // console.log(value);
+    console.log(rates.conversion_rates);
+    // console.log(price);
+    console.log(result);
 
-    console.log(rates[0].cc);
+    setToPrice(result);
     setFromPrice(value);
-    // setToPrice(result);
   };
+
+  // const onChangeFromCurrency = (cur) => {
+  //   setFromCurrency(cur);
+  //   onChangeFromPrice(fromPrice);
+  // };
 
   const onChangeToPrice = (value) => {
+    const result =
+      (rates.conversion_rates[fromCurrency] /
+        rates.conversion_rates[toCurrency]) *
+      value;
+
+    console.log(result);
+
+    setFromPrice(result);
     setToPrice(value);
   };
-
-  // console.log(rates[0].txt);
 
   return (
     <div className="App">
--- src/services/api.js
+++ src/services/api.js
@@ -1,13 +1,37 @@
 import axios from "axios";
 
+// const API_KEY = "c1jA4Omc0aB9I4607zOS7fR8s3XE8oF6"; // for - https://apilayer.com/marketplace/exchangerates_data-api#endpoints
+
+const API_KEY = "a6fe167cd6f918d1bf0caaf9";
+
 const instance = axios.create({
-  baseURL: "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?",
+  baseURL: "https://v6.exchangerate-api.com",
 });
 
-export const searchRates = async () => {
-  const { data } = await instance.get("json");
+export const searchRates = async (baseCode) => {
+  const { data } = await instance.get(`/v6/${API_KEY}/latest/${baseCode}`);
 
   console.log(data);
 
   return data;
 };
+
+// const instance = axios.create({
+//   baseURL: "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?",
+// });
+
+// export const searchRates = async () => {
+//   const { data } = await instance.get("json");
+
+//   console.log(data);
+
+//   return data;
+
+// export const searchRates = async (to = "GBP", from = "JPY", amount = 100) => {
+//   const { data } = await instance.get("/convert?", {
+//     params: {
+//       to: to,
+//       from: from,
+//       amount: amount,
+//     },
+//   });
